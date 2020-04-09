31dfd3ff8a67ac3aab76ce178d24560094fef3b4
Praful Vanker <pmvanker@gmail.com>
2020-04-09 11:45:41 +0530

    ldd

---
+++ Ldd/char_driver/chr_drv_ex1.c
@@ -0,0 +1,132 @@
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kdev_t.h>
+#include<linux/fs.h>
+#include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/slab.h>
+#include<linux/uaccess.h>
+
+#define mem_size 1024
+
+dev_t dev = 0;
+static struct cdev my_cdev;
+static struct class *dev_class;
+uint8_t *kernel_buffer = NULL;
+
+/* function prototype */
+static int __init chr_driver_init(void);
+static int __exit chr_driver_exit(void);
+
+/*
+static int my_open(struct inod *inod, struct file *file);
+static int my_release(struct inod *inod, struct file *file);
+static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+*/
+
+static int my_open(struct inod *inod, struct file *file)
+{
+    /* creating physical memory */
+    if( (kernel_buffer =  kmalloc(mem_size, GFP_KERNEL)) == 0)
+    {
+        printk(KERN_INFO"Can not allocate memory to kernel\n");
+        return -1;
+    }
+        printk(KERN_INFO"Device file Open\n");
+        return 0;
+}
+
+static int my_release(struct inod *inod, struct file *file)
+{
+    kfree(kernel_buffer);
+    printk(KERN_INFO"Device file closed\n");
+    return 0;
+}
+
+static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+    copy_to_user(buf, kernel_buffer, mem_size);
+    printk(KERN_INFO"Data read done\n");
+    return mem_size;
+}
+
+static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off)
+{
+    copy_from_user(kernel_buffer, buf, len);
+    printk(KERN_INFO"Data writen succsefully\n");
+    return len;
+}
+
+
+
+static struct file_operations fops;
+/*
+static struct file_operations fops ={
+    .owner = THIS_MODULE,
+    .read  =   my_read,
+    .write =   my_write,
+    .open  =   my_open,
+    .release =   my_release,
+};
+*/
+
+
+static int __init chr_driver_init(void)
+{
+    /* allocating the major no */
+    if( (alloc_chrdev_region(&dev, 0, 1, "my_dev")) < 0){
+        printk(KERN_INFO"Can not allocate Major Number\n");
+        return -1;
+    }
+    printk(KERN_INFO"Major = %d Minor=%d\n",MAJOR(dev),MINOR(dev));
+
+    /* creating cdev structure */
+    cdev_init(&my_cdev, &fops);
+
+    /* Adding charecter Device to system */
+    if( cdev_add(&my_cdev, dev, 1) < 0 ){
+        printk(KERN_INFO"Can not add device to system\n");
+        goto r_class;
+    }
+    
+    /* create strcut class */
+    if( (dev_class = class_create(THIS_MODULE, "my_class")) == NULL)
+    {
+        printk(KERN_INFO"Can not allocate struct class\n");
+        goto r_class;
+    }
+    
+    /* creating device */
+    if( (device_create(dev_class,NULL,dev,NULL,"my_device"))==NULL)
+    {
+        printk(KERN_INFO"Can not create device\n");
+        goto r_device;
+    }
+
+    printk(KERN_INFO"Device Driver insert done\n");
+    return 0;
+
+r_device:class_destroy(dev_class);  
+r_class:
+    unregister_chrdev_region(dev,1);
+    return -1;
+}
+
+
+static int __exit chr_driver_exit(void)
+{
+   device_destroy(dev_class, dev);
+   class_destroy(dev_class);
+   cdev_del(&my_cdev);
+   unregister_chrdev_region(dev,1);
+   printk(KERN_INFO"Device Driver removed succesfully\n");
+   return 0;
+}
+
+module_init(chr_driver_init);
+module_exit(chr_driver_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Praful Vanker");
+MODULE_DESCRIPTION("basic character device driver");
